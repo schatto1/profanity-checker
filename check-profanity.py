a06376ca76fd49605978829b7c0b4a052e67d08e
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-17 17:22:02 -0400

    Initial commit.
    Adds file reading capability to program.

---
+++ check-profanity.py
@@ -0,0 +1,11 @@
+def read_text():
+    input_file = r"/home/dzshu49/python-foundations/profanity-checker/movie_quotes.txt" # Replace with file path of file to be read
+    quotes = open(input_file) 
+    contents_of_files = quotes.read()
+    print(contents_of_files)
+    quotes.close()
+
+def check_profanity(text_to_check)
+
+
+read_text()
